2362c4d683e562cef1dd2b37f97d9abc8b1233f1
manjoeso <jmanso93@gmail.com>
2021-11-02 16:24:06 -0400

    finished 05

--- skeleton/lib/05_aggregates.rb
+++ skeleton/lib/05_aggregates.rb
@@ -44,18 +44,38 @@ end
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+    SELECT 
+      count(*) 
+    FROM 
+      countries
+    WHERE 
+      area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+    SELECT 
+      SUM(population)
+    FROM 
+      countries
+    WHERE 
+      name LIKE 'France' OR 
+      name LIKE 'Germany' OR 
+      name LIKE 'Spain';
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+    SELECT 
+      continent, count(name)
+    FROM
+      countries
+    GROUP BY 
+      continent;
   SQL
 end
 
@@ -63,11 +83,27 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+    SELECT 
+      continent, count(name)
+    FROM
+      countries
+    WHERE 
+      population >= 10000000
+    GROUP BY 
+      continent;
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+    SELECT 
+      continent
+    FROM 
+      countries
+    GROUP BY 
+      continent
+    HAVING
+      sum(population) >= 100000000;
   SQL
 end
